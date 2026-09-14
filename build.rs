8f9ad45ed52f07b05b1058ea830beb1f43249cfe
walle-2017 <1220754120@qq.com>
2026-09-14 13:29:06 +0800

    build: link Windows Composition gaussian blur helper

--- build.rs
+++ build.rs
@@ -3,6 +3,21 @@ use winres::{VersionInfo, WindowsResource};
 fn main() {
     let version = env!("CARGO_PKG_VERSION");
 
+    if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows") {
+        cc::Build::new()
+            .cpp(true)
+            .file("native/composition_blur.cpp")
+            .flag_if_supported("/std:c++20")
+            .flag_if_supported("/EHsc")
+            .compile("codex_composition_blur");
+
+        println!("cargo:rerun-if-changed=native/composition_blur.cpp");
+        println!("cargo:rustc-link-lib=windowsapp");
+        println!("cargo:rustc-link-lib=CoreMessaging");
+        println!("cargo:rustc-link-lib=runtimeobject");
+        println!("cargo:rustc-link-lib=d2d1");
+    }
+
     // Embed the icon and richer PE version metadata into the executable.
     let mut res = WindowsResource::new();
     let numeric_version = pack_version(version);
