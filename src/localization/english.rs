a32b8f50e8bc9b692c778013b584d291b7543652
walle-2017 <1220754120@qq.com>
2026-09-16 15:44:32 +0800

    ui: rename settings menu to other settings

--- src/localization/english.rs
+++ src/localization/english.rs
@@ -9,7 +9,7 @@ pub(super) const STRINGS: Strings = Strings {
     fifteen_minutes: "15 Minutes",
     one_hour: "1 Hour",
     codex_model: "Codex",
-    settings: "Settings",
+    settings: "Other settings",
     start_with_windows: "Start with Windows",
     reset_position: "Reset Position",
     language: "Language",
